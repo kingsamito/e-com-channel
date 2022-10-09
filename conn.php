930d49fa281321dc95f62b84bc4e4c84ca5c8078
kingsamito <66538425+kingsamito@users.noreply.github.com>
2022-10-09 23:26:16 +0100

    Update conn.php

--- conn.php
+++ conn.php
@@ -1,9 +1,9 @@
 <?php
 
-$dbsevername = "localhost";
-$dbusername = "root";
-$dbpassword = "";
-$dbname = "memo_system";
+$dbsevername = "sql306.epizy.com";
+$dbusername = "epiz_32759108";
+$dbpassword = "ekdx7lLM85VWs";
+$dbname = "epiz_32759108_e_com_system";
 
 $conn = mysqli_connect($dbsevername, $dbusername, $dbpassword, $dbname) or die("Database connection failed");
 
