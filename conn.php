833100afdac130d49a3bf9d2858e7994dc81e7d7
kingsamito <66538425+kingsamito@users.noreply.github.com>
2022-10-09 23:31:09 +0100

    Update conn.php

--- conn.php
+++ conn.php
@@ -5,6 +5,8 @@ $dbusername = "epiz_32759108";
 $dbpassword = "ekdx7lLM85VWs";
 $dbname = "epiz_32759108_e_com_system";
 
-$conn = mysqli_connect($dbsevername, $dbusername, $dbpassword, $dbname) or die("Database connection failed");
-
+$conn = mysqli_connect($dbsevername, $dbusername, $dbpassword, $dbname);
+if (!$conn) {
+  die("Connection failed: " . mysqli_connect_error());
+}
 ?>
